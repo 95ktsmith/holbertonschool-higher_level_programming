b921bd8ea86f7af65be899b2449ba147304f2a3f
Kevin Smith <95ktsmith@gmail.com>
2020-04-28 17:43:04 +0000

    Initial commit.

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/ur/bin/python3
+def pow(a, b):
+    result = 1
+
+    if b < 0:
+        for pows in range(b, 0):
+            result *= a
+        return 1 / result
+    else:
+        for pows in range(0, b):
+            result *= a
+        return result
