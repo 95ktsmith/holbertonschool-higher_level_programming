ec1c7b743da066fd96a11d2135f70e603dc90310
Kevin Smith <95ktsmith@gmail.com>
2020-04-29 01:15:41 +0000

    Reordered if/elif/else.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,9 +10,9 @@ else:
     digit = number % 10
 print("is {:d}".format(digit), end=" ")
 
-if digit == 0:
-    print("and is 0")
-elif digit < 6:
-    print("and is less than 6 and not 0")
-else:
+if digit > 5:
     print("and is greater than 5")
+elif digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
