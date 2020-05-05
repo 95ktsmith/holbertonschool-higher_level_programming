3ea1481898280c79a35ee93aeb03f47291250c37
Kevin Smith <95ktsmith@gmail.com>
2020-05-05 20:46:10 +0000

    Changed variable name for clarity.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for elem in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[elem]))
+    for index in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[index])
