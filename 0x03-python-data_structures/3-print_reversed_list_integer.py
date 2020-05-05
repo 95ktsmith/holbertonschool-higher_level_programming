e75b5eb0a987af432078877334104cb443ee061e
Kevin Smith <95ktsmith@gmail.com>
2020-05-05 20:49:16 +0000

    Added a closing parenthesis at the end of line 6.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for index in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[index])
+    for index in range(len(my_list), 0, -1):
+        print("{:d}".format(my_list[index - 1]))
