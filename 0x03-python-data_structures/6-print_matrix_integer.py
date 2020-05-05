62bab922bcadb35311c0eead2aefef389e416043
Kevin Smith <95ktsmith@gmail.com>
2020-05-05 22:01:58 +0000

    Initial commit.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index in range(0, len(row)):
+            if index == len(row) - 1:
+                print("{:d}".format(row[index]), end="")
+            else:
+                print("{:d}".format(row[index]), end=" ")
+        print("")
