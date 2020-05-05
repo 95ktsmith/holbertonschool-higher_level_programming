ffd6d2935e6758467845819dcafd76d79441a928
95ktsmith <95ktsmith@gmail.com>
2020-05-05 23:00:23 +0000

    Start largest_int at first number of list instead of 0.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    largest_int = 0
+    largest_int = my_list[0]
     for num in my_list:
         if num > largest_int:
             largest_int = num
