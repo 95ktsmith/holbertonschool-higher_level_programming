8a60858bb0ef14220805f41aa506ba3678d4e5af
95ktsmith <95ktsmith@gmail.com>
2020-05-18 17:41:39 +0000

    Initial commit.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+	elemsPrinted = 0
+	while elemsPrinted < x:
+		try:
+			print(my_list[elemsPrinted], end="")
+			elemsPrinted += 1
+		except:
+			break
+	print("")
+	return elemsPrinted
