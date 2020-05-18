2278b98f798a5b30ab22eaffc865d1f517d3fc82
95ktsmith <95ktsmith@gmail.com>
2020-05-18 18:16:02 +0000

    Used IndexError instead of a catch-all.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
-        except:
+        except IndexError:
             print("out of range")
             break
         finally:
