a7ce0938b5cb45caefa1b219fcb60b0835edc738
95ktsmith <95ktsmith@gmail.com>
2020-05-18 18:21:47 +0000

    Combined ValueError and IndexError, and loop does not stop if an IndexError is caught.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,11 +8,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             elemsPrinted += 1
+        except (ValueError, IndexError):
+            pass
+        finally:
             index += 1
-        except ValueError:
-            index += 1
-        except:
-            break
 
     print("")
     return elemsPrinted
