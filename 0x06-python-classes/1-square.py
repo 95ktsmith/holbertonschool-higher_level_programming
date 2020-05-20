0a0dbb970555f05bf5296c7e842937db05a382d4
95ktsmith <95ktsmith@gmail.com>
2020-05-20 13:20:21 +0000

    Added documentation.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+""" Square """
 
 
 class Square:
+    """ A Square
+
+    Attributes:
+        __size (int): Size of the square
+    """
     __size = 0
 
     def __init__(self, size):
