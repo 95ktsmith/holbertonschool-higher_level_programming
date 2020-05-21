4ee4e3b54db8d9fde423866e0878dd99fe9bab8b
95ktsmith <95ktsmith@gmail.com>
2020-05-21 16:04:43 +0000

    Added documentation.

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+""" Square """
 
 
 class Square:
+    """ Square
+        Attributes:
+            __size (int): Size of the square
+    """
     __size = 0
 
     def __init__(self, size=0):
+        """ Init """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -13,14 +19,17 @@ class Square:
             self.__size = size
 
     def area(self):
+        """ Returns area of the square """
         return self.__size ** 2
 
     @property
     def size(self):
+        """ Returns the size of the sqaure """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Sets the size of the square """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -29,6 +38,7 @@ class Square:
             self.__size = value
 
     def my_print(self):
+        """ Prints the square """
         if self.__size == 0:
             print("")
         for column in range(0, self.__size):
