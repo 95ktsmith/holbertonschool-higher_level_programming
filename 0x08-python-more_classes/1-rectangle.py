9d7d888e6f0c2c44f7a185a0255a48a0192c58f6
Kevin Smith <95ktsmith@gmail.com>
2020-05-26 15:41:00 +0000

    Correctly named height getter and setter.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -46,8 +46,8 @@ class Rectangle:
         """ Returns the height of the rectangle """
         return self.__height
 
-    @width.setter
-    def width(self, value):
+    @height.setter
+    def height(self, value):
         """ Sets the height of the rectangle """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
