85b83ced9c682244d1ff20a5898e65e7276645e4
95ktsmith <95ktsmith@gmail.com>
2020-05-27 13:49:08 +0000

    Initial commit.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Add integer """
+
+
+def add_integer(a, b=98):
+    """ Adds two integers and returns the result 
+        a and b must be integers or floats
+    """
+    import doctest
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
