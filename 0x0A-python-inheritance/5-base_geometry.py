2e7b1ccb857f1dff97d647ad0cf7803a5233a2a0
Kevin Smith <95ktsmith@gmail.com>
2020-06-02 21:32:27 +0000

    Initial commit.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Geometry base Class """
+
+
+class BaseGeometry:
+    """ BaseGeometry Class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry base Class """
+
+
+class BaseGeometry:
+    """ BaseGeometry Class """
+    def area(self):
+        """ Prints area """
+        raise Exception("area() is not implemented")
