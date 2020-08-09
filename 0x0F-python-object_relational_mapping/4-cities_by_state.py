f711c63632717f75c3005cb8f9bda42a9e467854
95ktsmith <95ktsmith@gmail.com>
2020-08-09 20:15:04 +0000

    No longer uses a sub-query.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name,\
-                 (SELECT name FROM states WHERE id=cities.state_id)\
-                 FROM cities RIGHT JOIN states ON cities.state_id = states.id\
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities, states WHERE cities.state_id = states.id\
                  ORDER BY cities.id ASC")
 
     query_rows = cur.fetchall()
