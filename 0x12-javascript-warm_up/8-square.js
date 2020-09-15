bbcb58bef4be2e0fb0593e749b71441d24f45614
Kevin Smith <95ktsmith@gmail.com>
2020-09-15 19:22:06 +0000

    Added check if argument is not a number.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const size = process.argv.slice(2);
-if (size.length === 0) {
+if (size.length === 0 || isNaN(size[0])) {
   console.log('Missing size');
 } else {
   for (let row = 0; row < size[0]; row++) {
