cbea32b5d142b10ce39242585b69329bcc6576b0
95ktsmith <95ktsmith@gmail.com>
2020-09-22 23:30:28 +0000

    Moved scraping into an else block after check.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,16 +5,17 @@ request({ url: url, method: 'GET' },
   function (error, response, body) {
     if (error) {
       console.log(error);
-    }
-    const info = JSON.parse(body);
-    let count = 0;
-    for (let index = 0; index < info.count; index++) {
-      for (let cIndex = 0; cIndex < info.results[index].characters.length; cIndex++) {
-        if (info.results[index].characters[cIndex].includes('18')) {
-          count++;
-          break;
+    } else {
+      const info = JSON.parse(body);
+      let count = 0;
+      for (let index = 0; index < info.count; index++) {
+        for (let cIndex = 0; cIndex < info.results[index].characters.length; cIndex++) {
+          if (info.results[index].characters[cIndex].includes('18')) {
+            count++;
+            break;
+          }
         }
       }
+      console.log(count);
     }
-    console.log(count);
   });
