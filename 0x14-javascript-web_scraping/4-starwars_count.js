38b6d29239d8bd4b3abcd35eb4d3714ebbe45d54
95ktsmith <95ktsmith@gmail.com>
2020-09-22 23:00:53 +0000

    Initial commit.

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+request({ url: 'https://swapi-api.hbtn.io/api/films', method: 'GET' },
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    const info = JSON.parse(body);
+    let count = 0;
+    for (let index = 0; index < info.count; index++) {
+      for (let cIndex = 0; cIndex < info.results[index].characters.length; cIndex++) {
+        if (info.results[index].characters[cIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  });
