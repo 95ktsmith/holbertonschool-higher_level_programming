83cf1e4469b3f9c3f8d35b324c47ed908f8fc9fd
Kevin Smith <95ktsmith@gmail.com>
2020-09-23 13:50:01 +0000

    Initial commit.

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let char;
+    if (c == undefined) {
+      char = 'X';
+    } else {
+      char = c;
+    }
+    for (let h = 0; h < this.height; h++) {
+      let line = '';
+      for (let w = 0; w < this.width; w++) {
+        line += char;
+      }
+      console.log(line);
+    }
+  }
+}
